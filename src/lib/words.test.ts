0e468826632123a4d6662d404ee410119407ae9a
Jeremy Hummel <jmhummel@gmail.com>
2022-12-30 00:55:36 -0500

    fix tests

--- src/lib/words.test.ts
+++ src/lib/words.test.ts
@@ -72,8 +72,8 @@ describe('solutionIndex', () => {
 
   test('word of the day', () => {
     expect(() => getWordOfDay(-1)).toThrowError('Invalid index')
-    expect(getWordOfDay(0)).toEqual('WHICH')
-    expect(getWordOfDay(1)).toEqual('THERE')
-    expect(getWordOfDay(255)).toEqual('SHEEP')
+    expect(getWordOfDay(0)).toEqual('BUSINESS')
+    expect(getWordOfDay(1)).toEqual('RESEARCH')
+    expect(getWordOfDay(255)).toEqual('VELOCITY')
   })
 })
